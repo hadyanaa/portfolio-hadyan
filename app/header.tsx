3c5e4560cbcb8ab6aec0a28fb20d009dd42a4abc
Hadyan Abdul Aziz <hadyanabdulaziz@gmail.com>
2024-07-23 10:42:01 +0700

    add about and skill section

--- app/header.tsx
+++ app/header.tsx
@@ -3,7 +3,7 @@
 import { CiMenuFries } from "react-icons/ci";
 export default function Header(){
   return(
-    <div className="flex justify-between items-center bg-primary px-12 xl:px-24 py-4 h-12">
+    <div className="fixed top-0 right-0 z-50 duration-500 inset-x-0 flex justify-between items-center bg-primary px-12 xl:px-24 py-4 h-12 backdrop-blur-lg bg-opacity-60">
       <h1>Hadyan Abdul Aziz</h1>
       {/* desktop nav */}
       <div className="hidden xl:flex xl:flex-row xl:items-center xl:gap-x-4">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex bg-primary min-h-screen flex-col justify-between px-8 xl:px-20">
+    <main className="flex bg-primary min-h-screen flex-col justify-between px-8 xl:px-20 gap-y-16">
       {/* landing hero */}
-      <div className="flex flex-col xl:flex-row justify-center items-center gap-8 mt-12 xl:mt-40">
+      <div className="flex flex-col xl:flex-row justify-center items-center gap-8 mt-12 xl:mt-40 mb-24">
         <div className="flex flex-col justify-center items-center xl:items-start text-center xl:text-left gap-4 order-2 xl:order-none">
           <span className="text-blue font-sans">Hi! my name is</span>
           <h1 className="text-4xl xl:text-5xl font-bold">Hadyan Abdul Aziz.</h1>
@@ -18,8 +18,24 @@ export default function Home() {
           <Image src="/images/hadyan.png" alt="My Photo" width="0" height="0" sizes="100vw" className="w-[150px] h-auto xl:w-[400px]" />
         </div>
       </div>
+      {/* about */}
+      <div className="flex flex-col gap-4 mt-12 xl:mt-40 mb-24">
+        <h1 className="text-xl xl:text-2xl font-normal">About Me</h1>
+        <p className="text-gray font-sans text-base xl:text-lg">I graduated with a bachelor's degree in information system from  
+          <span className="text-blue"> Sekolah Tinggi Teknologi Terpadu Nurul Fikri</span> in March 2023, where I gained experience
+          in various programming languages and tools, with a focus on frontend development. During my studies, I completed several 
+          projects using React, including a responsive e-commerce website. I also developed a custom content management system for a 
+          client using Laravel. I am passionate about learning and growing in the field of frontend development, and I am constantly 
+          seeking out new challenges and opportunities to expand my skills. I have a particular interest in React, and I have completed 
+          multiple certifications and courses related to this framework. I also have skills in Figma, a software for designing user interfaces, 
+          and SQL, a language for managing databases.
+        </p>
+      </div>
 
-      <div className="h-40"></div>
+      {/* skills */}
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-xl xl:text-2xl font-normal">What I Do</h1>
+      </div>
     </main>
   );
 }
