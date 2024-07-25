4b8dbcfdf05007bddd1e2e9f9011de11dc9db6d5
Hadyan Abdul Aziz <hadyanabdulaziz@gmail.com>
2024-07-25 18:10:48 +0700

    update error

--- components/homes/contacts.tsx
+++ components/homes/contacts.tsx
@@ -3,7 +3,7 @@ import Button from "../atoms/button";
 export default function Contacts(){
   return (
     <div id="contacts" className="flex flex-col justify-center items-center gap-8 mt-12 pt-20 xl:mt-40 mb-52">
-      <span className="text-blue font-sans">What is next</span>
+      <span className="text-blue font-sans">What &apos;s next</span>
       <h1 className="text-4xl xl:text-5xl font-bold">Get In Touch</h1>
       <p className="text-gray font-sans text-sm xl:text-base text-center max-w-[70%] leading-relaxed">
       I am currently looking for a remote job or any new opportunities. Whether you have a project to discuss or just want to say hi, my inbox is open for all!
