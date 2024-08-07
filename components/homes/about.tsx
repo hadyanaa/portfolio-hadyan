5a1f04d1e1c1d4b6c23a7d50b2e364f9ff263256
Hadyan Abdul Aziz <hadyanabdulaziz@gmail.com>
2024-08-07 08:22:48 +0700

    update resume and about

--- components/homes/about.tsx
+++ components/homes/about.tsx
@@ -2,14 +2,12 @@ export default function About(){
   return(
     <div id="about" className="flex flex-col gap-4 mt-12 pt-20 xl:mt-40 mb-24">
       <h1 className="text-xl xl:text-2xl font-normal">About Me</h1>
-      <p className="text-gray font-sans text-sm xl:text-base text-justify leading-relaxed">I graduated with a bachelor&apos;s degree in information system from  
-        <span className="text-blue"> Sekolah Tinggi Teknologi Terpadu Nurul Fikri</span> in March 2023, where I gained experience
-        in various programming languages and tools, with a focus on frontend development. During my studies, I completed several 
-        projects using React, including a responsive e-commerce website. I also developed a custom content management system for a 
-        client using Laravel. I am passionate about learning and growing in the field of frontend development, and I am constantly 
-        seeking out new challenges and opportunities to expand my skills. I have a particular interest in React, and I have completed 
-        multiple certifications and courses related to this framework. I also have skills in Figma, a software for designing user interfaces, 
-        and SQL, a language for managing databases.
+      <p className="text-gray font-sans text-sm xl:text-base text-justify leading-relaxed">
+        Graduated with a Bsc in Information System from <span className="text-blue"> Sekolah Tinggi Teknologi Terpadu Nurul Fikri</span> in March 2023. 
+        Have more than a year of professional experience in technology industry.
+        Currently working as a junior developer that was placed at PT Indosat Tbk. 
+        Committed to advancing knowledge in project development and other technologies to have advantages in the future. 
+        Passionate learning and growing to build career professional.
       </p>
     </div>
   )
